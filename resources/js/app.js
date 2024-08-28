adc9b23a9e3a6f13b477898269f2a2366f47087a
unknown <jnc201820@gmail.com>
2024-08-28 00:06:57 -0300

    chore: install Inertia.js and configure Vue 3 integration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 import './bootstrap';
+
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/index";
+
+createInertiaApp({
+    resolve: name => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: "#FF9900"
+    }
+});
---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html class="h-full" lang="pt-br">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <title>Plataforma</title>
+    @routes
+    @vite(['resources/js/app.js', 'resources/css/app.css','resources/css/resources.scss'])
+    @inertiaHead
+</head>
+
+<body>
+    @inertia
+</body>
+
+</html>
